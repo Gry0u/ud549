6824d42a04efa125ebe8726530aa3e53b7d33659
Gry0u <gauthriou@protonmail.com>
2018-09-22 17:06:59 +0200

    Pass asynchronous test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -14,3 +14,17 @@ describe('Address Book', function() {
     expect(addressBook.getContact(0)).not.toBeDefined();
   });
 });
+
+describe('Async address book', function() {
+  let addressBook = new AddressBook();
+  beforeEach(function(done) {
+    addressBook.getInitialContacts(function() {
+      done();
+    });
+  });
+
+  it('should grab initial contacts', function(done) {
+    expect(addressBook.initialComplete).toBe(true);
+    done();
+  });
+});
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,11 +1,25 @@
 function AddressBook() {
   this.contacts = [];
+  this.initialComplete = false;
+
+  this.getInitialContacts = function(cb) {
+    let self = this;
+    setTimeout(function() {
+      self.initialComplete = true;
+      if (cb) {
+        return cb();
+      }
+    }, 3);
+  }
+
   this.addContact = function(contact) {
     this.contacts.push(contact);
   }
+
   this.deleteContact = function(index) {
     this.contacts.splice(index, 1);
   }
+
   this.getContact = function(index) {
     return this.contacts[index];
   }
